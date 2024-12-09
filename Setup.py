2e18e92e61ab1a44941b7a5c7e017ee8ae15ec1f
ABHAYHBB <pathakabhay360@gmail.com>
2024-12-09 15:18:50 +0530

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -1,1 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gquant',
+    version='0.1.0',
+    author='Abhay Kumar Pathak',
+    author_email='pathakabhay@bhu.ac.in',
+    description='A Python package for preprocessing, analysis, and visualization',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/username/gQuant-Tool',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'matplotlib',
+        'seaborn'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
 
